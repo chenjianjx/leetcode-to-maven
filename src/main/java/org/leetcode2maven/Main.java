3ddc5a7077ad9a7834d3b84bfa9463fbd942819c
Jian Chen <chenjianjx@gmail.com>
2022-11-05 17:19:03 +0800

    commit before removing the graphql generator

--- src/main/java/org/leetcode2maven/Main.java
+++ src/main/java/org/leetcode2maven/Main.java
@@ -1,7 +1,25 @@
 package org.leetcode2maven;
 
+import com.graphql_java_generator.exception.GraphQLRequestExecutionException;
+import com.graphql_java_generator.exception.GraphQLRequestPreparationException;
+import org.leetcode2maven.graphqlclient.QuestionInput;
+import org.leetcode2maven.graphqlclient.QuestionNode;
+import org.leetcode2maven.graphqlclient.util.QueryExecutor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws GraphQLRequestPreparationException, GraphQLRequestExecutionException {
+        QueryExecutor queryExecutor = new QueryExecutor("https://leetcode.com/graphql");
+
+
+        QuestionNode questionNode = queryExecutor.questionData("{questionId title titleSlug}",
+                QuestionInput.builder()
+                        .withTitleSlug("tow-sum")
+                        .build());
+        System.err.println(questionNode);
+
+
     }
 }
