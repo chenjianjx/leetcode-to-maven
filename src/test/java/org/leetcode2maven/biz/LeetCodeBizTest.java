fa5be8e96307db0cc0386e93a7fd2460cd9acf95
Jian Chen <chenjianjx@gmail.com>
2022-11-06 18:39:30 +0800

    Generate test better

--- src/test/java/org/leetcode2maven/biz/LeetCodeBizTest.java
+++ src/test/java/org/leetcode2maven/biz/LeetCodeBizTest.java
@@ -51,15 +51,16 @@ class LeetCodeBizTest {
         Question question = new Question();
         question.setDefaultTestCases(
                 Arrays.asList(
-                        TestCase.builder().input(new ArrayList<>(Arrays.asList("[2,7,11,15]"))).expected("9").build(),
-                        TestCase.builder().input(new ArrayList<>(Arrays.asList("[3,2,4]"))).expected("6").build(),
-                        TestCase.builder().input(new ArrayList<>(Arrays.asList("[3,3]"))).expected("6").build()
+                        TestCase.builder().input(new ArrayList<>(Arrays.asList("[2,7,11,15]", "9"))).expected("[0,1]").build(),
+                        TestCase.builder().input(new ArrayList<>(Arrays.asList("[3,2,4]", "6"))).expected("[1,2]").build(),
+                        TestCase.builder().input(new ArrayList<>(Arrays.asList("[3,3]", "6"))).expected("[0,1]").build()
                 )
         );
         SingleClassCode result = biz.buildUnitTestCode(parseResult, question);
 
+
         assertEquals("SolutionTest", result.getClassName());
-        assertEquals("wa ha ha", result.getSource());
+        assertEquals(IOUtils.toString(this.getClass().getResource("/leetcode-1-generated-test.txt"), DEFAULT_CHARSET), result.getSource());
     }
 
 }
